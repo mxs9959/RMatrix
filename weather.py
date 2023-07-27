6093ce0cc451d1c6bb6d953994d3c2be7a6508c5
Max <maxscholle56@gmail.com>
2023-07-26 21:47:15 -0400

    Adding weather icons

--- weather.py
+++ weather.py
@@ -3,18 +3,33 @@ import requests
 import json
 import datetime
 from time import sleep
-import threading
+import PIL
 import matrix_tools
 from rgbmatrix import graphics
 
 m = matrix_tools.getMatrix()
+c = m.CreateFrameCanvas()
 font0507 = graphics.Font()
 font0507.LoadFont('/home/mxs/rpi-rgb-led-matrix/fonts/5x7.bdf')
 font0406 = graphics.Font()
 font0406.LoadFont('/home/mxs/rpi-rgb-led-matrix/fonts/4x6.bdf')
 softWhite = graphics.Color(200, 200, 200)
 
-#There are two methods to handle data: refresh and load. Refresh retrieves data from Open-Meteo API, so it will be used infrequently and only by user command. Refresh stores the data to a JSON file. Load retrieves the data and stores it as a dictionary in the variable data.
+s = '/home/mxs/RMatrix/thumbnails/'
+icons = [
+PIL.Image.open(s+'sunny.png'),
+PIL.Image.open(s+'pcloudy.png'),
+PIL.Image.open(s+'cloudy.png'),
+PIL.Image.open(s+'lrain.png'),
+PIL.Image.open(s+'hrain.png'),
+PIL.Image.open(s+'snow.png'),
+PIL.Image.open(s+'fog.png'),
+PIL.Image.open(s+'stormy.png')
+]
+for obj in icons:
+	obj.thumbnail((20,20), PIL.Image.ANTIALIAS)
+	icons[icons.index(obj)] = obj.convert('RGB')
+
 def refresh():
 	print("WARNING: SENDING GET REQUEST FOR WEATHER DATA.")
 	response = requests.get('https://api.open-meteo.com/v1/forecast?latitude=33.749&longitude=-84.388&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,cloudcover&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York')
@@ -26,10 +41,6 @@ def load():
 	file_r = open('weather_data.json', 'r')
 	data = json.load(file_r)
 
-#hourly method generates a list of strings to be played when hourly data is displayed.
-#The ticker is primarily based on timestamps. Timestamps are gathered from the data and converted into prettier forms.
-#An assortment of hourly data points is available. The desired data is collected and joined with a unit, a label, and ultimately the timestamp that corresponds to it.
-#The final ticker strings are stored in a list. Timestamps are displayed in different colors from their corresponding data.
 def hourly():
 	global ticker
 	global days
@@ -37,7 +48,6 @@ def hourly():
 	days = []
 	hourly = data['hourly']
 	for time in hourly['time']:
-		#Creating timestamp for ticker
 		hr = int(time[11:13])
 		if hr > 12:
 			hr = str(hr-12) + "PM: "
@@ -64,7 +74,6 @@ def hourly():
 		else:
 			hr = str(hr) + "AM: "
 
-		#Creating data list
 		item = hourly['time'].index(time)
 		info = ""
 		for point in hourly:
@@ -80,11 +89,9 @@ def hourly():
 				info += "Cover: " + str(hourly[point][item]) + "% "
 		ticker.append(info)
 
-#stickScroll displays a list of strings on the matrix by scrolling them with alternating colors. The headers will stick to the side until replaced.
-def stickScroll():
+def display():
 	global thisday
-	global draw
-	draw = False
+	global c
 	color1 = matrix_tools.randVisColor(100)
 	color2 = matrix_tools.randVisColor(100)
 	colors = [graphics.Color(color1[0], color1[1], color1[2]), graphics.Color(color2[0], color2[1], color2[2])]
@@ -95,9 +102,12 @@ def stickScroll():
 	stuck = None
 	thisday = 0
 	blank = graphics.Color(0,0,0)
+	slide = 0
+	nextSlide = False
 	Y = 30
+	infoX = 22
+	infoY = 14
 
-	#Displaying strings and managing which are displayed.
 	while len(displaying) > 0:
 		for desc in displaying:
 			if desc[1]-pos == 1 and desc[0]%2 == 0:
@@ -106,7 +116,7 @@ def stickScroll():
 				desc[1] += 1
 				stuck = desc
 			else:
-				length = graphics.DrawText(m, font0507, desc[1]-pos, Y, colors[desc[0]%2], ticker[desc[0]])
+				length = graphics.DrawText(c, font0507, desc[1]-pos, Y, colors[desc[0]%2], ticker[desc[0]])
 				desc[2] = length
 			if displaying.index(desc) == len(displaying)-1 and desc[1]+length-pos < 64:
 				displaying.append([desc[0]+1, desc[1] + length, 0])
@@ -115,35 +125,35 @@ def stickScroll():
 			if length + desc[1] < pos:
 				remove = desc
 		if stuck != None:
-			matrix_tools.fillRect(m, 0, 23, stuck[2]-4, 7, blank)
-			graphics.DrawText(m, font0507, 1, Y, colors[0], ticker[stuck[0]])
+			matrix_tools.fillRect(c, 0, 23, stuck[2]-4, 7, blank)
+			graphics.DrawText(c, font0507, 1, Y, colors[0], ticker[stuck[0]])
 			stuck = None
-		graphics.DrawLine(m, 0, 22, 64, 22, softWhite)
-		draw = True
-		sleep(0.05) #Keep over the daily method draw checking period
-		pos += 1
-		m.Clear()
-		draw = False
-		if not remove == None:
+		if remove != None:
 			displaying.remove(remove)
 			remove = None
+		pos += 1
 
-#daily displays the daily weather information at the top of the matrix. It cycles through messages.
-def daily():
-	slide = 0
-	while True:
-		while not draw:
-			sleep(0.02)
-		graphics.DrawText(m, font0507, 49, 7, softWhite, days[thisday])
+		graphics.DrawLine(c, 0, 22, 64, 22, softWhite)
+		graphics.DrawText(c, font0507, 49, 7, softWhite, days[thisday])
 		if slide == 0:
-			graphics.DrawText(m, font0406, 20, 15, softWhite, "Information")
+			graphics.DrawText(c, font0406, infoX, infoY, softWhite, "Information")
 		elif slide == 1:
-			graphics.DrawText(m, font0406, 20, 15, softWhite, "Fiddles")
+			graphics.DrawText(c, font0406, infoX, infoY, softWhite, "Fiddles")
 		if datetime.datetime.now().second%5 == 0:
-			if slide < 1:
-				slide += 1
-			else:
-				slide = 0
+			if not nextSlide:
+				nextSlide = True
+				if slide < 1:
+					slide += 1
+				else:
+					slide = 0
+		else:
+			nextSlide = False
+
+		c.SetImage(icons[round(datetime.datetime.now().second/8)], 1,1)
+
+		c = m.SwapOnVSync(c)
+		sleep(0.05)
+		m.Clear()
 
 if __name__ == '__main__':
 	print("Welcome to Max Scholle's Weather Display.")
@@ -155,7 +165,4 @@ if __name__ == '__main__':
 	load()
 	hourly()
 	print("Displaying the weather...")
-	scroll = threading.Thread(target=stickScroll)
-	top = threading.Thread(target=daily)
-	scroll.start()
-	top.start()
+	display()
